f780a38b92005c9b810eaec1e3b88152a572a60a
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2022-09-29 11:55:42 -0500

    Controlled the todoInput component

--- src/Components/TodoInput.js
+++ src/Components/TodoInput.js
@@ -1,14 +1,24 @@
+import {useState} from 'react'
 const TodoInput = () => {
+
+    const [text, setText] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        console.log(text)
         // be able to log out the input value when submitted.
         // research controlled components / forms withing react
     }
 
+    const handleChange = (e) => {
+        setText(e.target.value)
+    }
+
+
+
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" />
+            <input type="text" value={text} onChange={handleChange}/>
             <input type="submit" />
         </form>
     )
