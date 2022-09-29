f832c15031dd20f36d8a9ad00b39fea268b9cae1
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2022-09-29 14:17:32 -0500

    Controlled component

--- src/Components/TodoInput.js
+++ src/Components/TodoInput.js
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+
 const TodoInput = () => {
 
     const [text, setText] = useState('')
@@ -6,19 +7,17 @@ const TodoInput = () => {
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log(text)
-        // be able to log out the input value when submitted.
-        // research controlled components / forms withing react
+        setText('')
     }
 
     const handleChange = (e) => {
         setText(e.target.value)
     }
 
-
-
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" value={text} onChange={handleChange}/>
+            {/* tie the value of the input to the LOCAL state */}
+            <input type="text" value={text} onChange={handleChange} />
             <input type="submit" />
         </form>
     )
